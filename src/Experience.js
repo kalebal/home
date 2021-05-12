2b39fddd72511d643b47db673675821a72ba1387
Kaleb Lyda <kaleblyda14@gmail.com>
2021-05-12 17:52:58 -0400

    Adds project info to collapsable list items

--- src/Experience.js
+++ src/Experience.js
@@ -1,51 +1,73 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function Experience({ refProp }) {
-  return(
-    <section id="exp" ref={refProp}>
-      <h2> 0.2 Some Things I've Done </h2>
-      <div className="exp-container">
-        <ul className="exp-ul">
-          <li className="exp-li">
-          <a href="https://github.com/kalebal/catwalk">Catwalk</a>
-          </li>
-          <li className="exp-li">
-            <a href="https://github.com/kalebal/Q-A">Atelier</a>
-          </li>
-          <li className="exp-li"><a href="https://github.com/kalebal/parkPack">Park Pack</a></li>
-          <li className="exp-li"><a href="https://devpost.com/software/fitfriends-0ohz73">Park Pack</a></li>
-        </ul>
+  const [projectData, setProjectData] = useState("");
 
-      </div>
-    </section>
-  );
+  // fetches local JSON data (in public folder)
+  useEffect(() => {
+    fetch('projects.json', {
+      headers : {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }}).then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      setProjectData(data);
+    })
+    .catch((err) => {
+      setProjectData(null);
+    });
+  }, []);
+
+  if (projectData) {
+    return(
+      <section id="exp" ref={refProp}>
+        <h2> 0.2 Some Things I've Done </h2>
+        <div className="exp-container">
+          <ul className="exp-ul">
+              {projectData.map((proj) => {
+                return (
+                  <li key={proj.name} className="exp-li">
+                    <Collapse
+                    name={proj.name}>
+                     <ProjectCard
+                        description={proj.description}
+                        link={proj.link}
+                        imgs={proj.imgs} />
+                    </Collapse>
+                  </li>
+                  );
+              })}
+
+          </ul>
+
+        </div>
+      </section>
+    );
+  } else {
+    return (
+      <section id="exp" ref={refProp}>
+        <h2> 0.2 Some Things I've Done </h2>
+        <div className="exp-container">
+          <p> Sorry, seems like there's an issue loading my project data. They're still up on Github though!</p>
+        </div>
+      </section>
+    );
+  }
 }
 
-
-/*
-<Collapse collapsed={false} name="Catwalk">
-            <ProjectCard />
-          </Collapse>
-*/
-function ProjectCard() {
-  const [isModal, setModal] = React.useState(false);
+function ProjectCard({name, description, imgs, link}) {
+  imgs = JSON.parse(imgs);
   return (
     <div className="project-card">
       <div className="project-card-text">
-        <ul>
-          <li>Doloremque impedit unde.</li>
-          <li> Sit veniam reprehenderit quis quas aut ullam voluptatum laborum et. Quis totam ut explicabo sed.</li>
-          <li>Doloremque impedit unde reprehenderit quis quas aut ullam volupta.</li>
-        </ul>
+          <p> {description}</p>
+           <button className="primary-button">
+           <a href={link}>Check it out </a></button>
       </div>
-      <div className="project-img">
-        <img src="http://placeimg.com/640/480/abstract" alt="" onClick={() => setModal(true)}></img>
-        <Modal isVisible={isModal}
-        title="Modal Title"
-        content={<img src="https://i.imgur.com/kVpvEhN.png" alt=""></img>}
-        onClose={() => setModal(false)}
-      />
-      </div>
+      {imgs[0] && <img className="project-img" src={imgs[0]} alt="" />}
+
     </div>
   );
 };
@@ -68,38 +90,5 @@ const Collapse = ({ name, collapsed = true, children }) => {
         {children}
       </div>
     </>
-  );
-};
-
-function Modal({ isVisible = false, title, content, footer, onClose }) {
-  const keydownHandler = ({ key }) => {
-    switch (key) {
-      case 'Escape':
-        onClose();
-        break;
-      default:
-    }
-  };
-
-  React.useEffect(() => {
-    document.addEventListener('keydown', keydownHandler);
-    return () => document.removeEventListener('keydown', keydownHandler);
-  });
-
-  return !isVisible ? null : (
-    <div className="modal" onClick={onClose}>
-      <div className="modal-dialog" onClick={e => e.stopPropagation()}>
-        <div className="modal-header">
-          <h3 className="modal-title">{title}</h3>
-          <span className="modal-close" onClick={onClose}>
-            &times;
-          </span>
-        </div>
-        <div className="modal-body">
-          <div className="modal-content">{content}</div>
-        </div>
-        {footer && <div className="modal-footer">{footer}</div>}
-      </div>
-    </div>
   );
 };
