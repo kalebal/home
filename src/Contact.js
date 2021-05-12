a7d13cdd95287af083be2a5591e0d5597b87f432
Kaleb Lyda <kaleblyda14@gmail.com>
2021-05-11 21:37:54 -0400

    adds links to socials

--- src/Contact.js
+++ src/Contact.js
@@ -1,10 +1,9 @@
 export default function Contact({ refProp }) {
   return (
-    <section ref={refProp}>
+    <section id="contact" ref={refProp}>
       <h2> 0.3 Contact </h2>
-      <p> Find me on LinkedIn and Github </p>
-      <p> or email me at: <br />
-      kaleblyda14 [at] gmail (dot) com -- no robots
+      <p> Find me on <a href="https://github.com/kalebal">Github</a> and <a href="https://www.linkedin.com/in/kaleb-lyda">LinkedIn</a> or email me at: <br />
+      kaleblyda14 [at] gmail (dot) com
       </p>
     </section>
   );
