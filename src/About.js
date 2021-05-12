9771247bbe9c48aa4fb8c44447ba8dd86320db51
Kaleb Lyda <kaleblyda14@gmail.com>
2021-05-11 21:37:00 -0400

    Adds line breaks for readability

--- src/About.js
+++ src/About.js
@@ -1,13 +1,13 @@
 export default function About({ refProp }) {
   return(
-    <section>
-      <div id="about-container" ref={refProp}>
+    <section id="about">
+      <div id="about-text" ref={refProp}>
         <h2> 0.1 About Me </h2>
         <p>
           Hello! My name is Kaleb Lyda and I enjoy creating beautiful things. My interest in software engineering started in 2018 when I used a Java library called Processing to make sketches for art projects.
-
+          <br />
           Now I’m using my background in art to think outside the box when crafting exciting experiences that live on the web.
-
+          <br />
           Here’s a few technologies I’ve been using recently:
         </p>
         <ul className="about-ul">
@@ -19,7 +19,7 @@ export default function About({ refProp }) {
         </ul>
       </div>
       <div className="photo-container">
-        <img id="profile-picture" alt="" src="http://placeimg.com/640/480/business" />
+        <img id="profile-picture" alt="" src="http://placeimg.com/640/480/people" />
       </div>
     </section>
   );
